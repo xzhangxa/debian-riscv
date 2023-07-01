a082305930cb163385a9de9353fec2d1eb7934ae
Xin Zhang <zhangxin.xa@gmail.com>
2023-07-01 17:19:23 +0800

    Auto setup x86 images network for QEMU

--- x86_64/run_qemu_bios.sh
+++ x86_64/run_qemu_bios.sh
@@ -2,4 +2,6 @@
 
 qemu-system-x86_64 \
 -machine q35 -nographic -m 2048 -smp 4 \
+-device virtio-net-pci,netdev=eth0 \
+-netdev user,id=eth0,hostfwd=:127.0.0.1:22222-:22 \
 -drive file=debian-stable-amd64.img,format=raw,if=virtio
--- x86_64/run_qemu_uefi.sh
+++ x86_64/run_qemu_uefi.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-#-machine q35 -nographic -m 2048 -smp 4 \
-
 qemu-system-x86_64 \
 -drive if=pflash,format=raw,unit=0,readonly=on,file=/usr/share/OVMF/OVMF_CODE.fd \
 -drive if=pflash,format=raw,unit=1,readonly=on,file=/usr/share/OVMF/OVMF_VARS.fd \
 -nographic -m 2048 -smp 4 \
+-device virtio-net-pci,netdev=eth0 \
+-netdev user,id=eth0,hostfwd=:127.0.0.1:22222-:22 \
 -drive file=debian-stable-amd64.img,format=raw,if=virtio
